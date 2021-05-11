496a9ce69bb3bb50351a9095fa794681fa47601c
Arafat Olayiwola <harof.dev@gmail.com>
2021-05-11 09:41:18 +0100

    still on frontend

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Hi Store");
+const toggleOpenBtn = document.getElementById('opened');
+const linksPage = document.getElementById('links-page');
+
+toggleOpenBtn.addEventListener('click', function(){
+    linksPage.classList.toggle('toggle-open');
+})
+
+const toggleCloseBtn = document.getElementById('closed');
+
+toggleCloseBtn.addEventListener('click', function(){
+    linksPage.classList.toggle('toggle-close')
+})
+
